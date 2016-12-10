08745e661e68bde2165949b8dd344af9e43c92e7
Robert Arnold <robertarnold391@gmail.com>
2016-12-10 21:16:04 +0000

    Created Credits Scene because I like to work backwards lol

---
+++ OneRoom/Assets/Code/CreditsScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScript : MonoBehaviour
+{
+
+    public float CreditsSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 gameObject.transform.Translate(Vector3.up * CreditsSpeed * Time.deltaTime);
+	}
+}
