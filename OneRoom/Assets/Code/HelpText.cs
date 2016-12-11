784b473c9ba259c0e831ff424f763afe7805ee4d
Robert Arnold <robertarnold391@gmail.com>
2016-12-11 21:54:44 +0000

    Help work

---
+++ OneRoom/Assets/Code/HelpText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HelpText : MonoBehaviour
+{
+
+    public bool ShowHelp;
+
+	// Use this for initialization
+	void Start ()
+	{
+        if(ShowHelp)
+	        StartCoroutine("Reveal");
+
+	   
+	}
+
+    IEnumerator Reveal()
+    {
+        yield return new WaitForSeconds(10);
+        gameObject.GetComponent<Text>().text = "Press F1 For Help";
+    }
+	
+
+}
